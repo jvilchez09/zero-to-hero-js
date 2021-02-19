c89672a4882242b3edf11bbdcae0831ab527232a
José Vílchez <jjva90@gmail.com>
2021-02-18 21:37:29 -0500

    checkpoint 56

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,7 +1,17 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = 23;
+const xsss = 23;
 
 if (condition) {
 }
+
+console.log();
+
+console.log(xsss);
+
+// Ledyba
+
+/**
+ * checkpoint 56 instaling node js
+ */
