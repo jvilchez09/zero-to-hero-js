170126833148c193ea70aceeae79fb8d5b9b38d8
José Vílchez <jjva90@gmail.com>
2021-03-31 00:05:27 -0500

    manejando eventos

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉Correcto number';
+
+document.querySelector('.number').textContent = 13;
+
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 23;
+*/
+
+const x = function () {
+  console.log(document.querySelector('.guess').value);
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔No number';
+  }
+});
+
+/**
+ * Checkpoint vid #74
+ */
