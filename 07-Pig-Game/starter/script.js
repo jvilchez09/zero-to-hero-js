549b3e32a47a5db53ea093aaec1627aca5ad84ef
José Vílchez <jjva90@gmail.com>
2021-04-17 22:04:07 -0500

    pig game checkpoint 83

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// selecting elements
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1'); // faster when selecting a lot of elements
+const diceELement = document.querySelector('.dice');
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceELement.classList.add('hidden');
