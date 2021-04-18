c26dac8a550a4ee145498d55da5fe0ae449c1e6d
José Vílchez <jjva90@gmail.com>
2021-04-17 22:16:37 -0500

    pig game checkpoint 84

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -2,9 +2,31 @@
 
 // selecting elements
 const score0Element = document.querySelector('#score--0');
+const current0Element = document.getElementById('current--0');
 const score1Element = document.getElementById('score--1'); // faster when selecting a lot of elements
 const diceELement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+let currentScore = 0;
 
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceELement.classList.add('hidden');
+
+//rolling dice functions
+btnRoll.addEventListener('click', function () {
+  //random rumber between 1-6
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //display dice
+  diceELement.classList.remove('hidden');
+  diceELement.src = `dice-${dice}.png`;
+
+  //check roll 1
+  if (dice !== 1) {
+    // add current score
+    currentScore += dice;
+    current0Element.textContent = currentScore;
+  }
+});
