85e42c20b81ef9c093dd8ed43908fbc1ff3f2652
José Vílchez <jvilchez@agngroup.net>
2021-05-25 18:50:11 -0600

    updating

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -4,7 +4,26 @@
 
 /**Checkpoint #93 */
 
+function calcAge (birthYear) {
+    const age = 2037 - birthYear;
+    
+    function printAge() {
+        const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+        console.log(output);
 
+        if(birthYear >= 1981 && birthYear <= 1996) {
+            const firstName = "Esteban";
+            var millennial = true;
+            const str = `Oh, and you are a millennial, ${firstName}`;
+            console.log(str);
+        }
+        // console.log(str); //block scope 
+        console.log(millennial); //function scope
+        console.log(firstName);
+    }
+    printAge();
+    return age;
+}
 
-/**
- */
+const firstName = "Jose";
+calcAge(1991);
