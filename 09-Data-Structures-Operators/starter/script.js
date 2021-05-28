b1b1cdc805bcc149b840113662984a1c9f8bf980
José Vílchez <jvilchez@agngroup.net>
2021-05-27 20:22:16 -0600

    destructuriong objects

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -21,4 +21,78 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery: function({starterIndex, mainIndex, time, address}){
+    console.log(`order recived! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address}  at  ${time}`);
+
+  }
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+/*
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const ca = arr[2];
+
+const [x,y,z] = arr;
+console.log(x, y, z);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+
+const [starter, mainCours] = restaurant.order(2, 0);
+console.log(starter, mainCours);
+
+//nested destructuring
+const nested = [2, 4, [5,6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//default values
+const [p=1, q=1, r=1] = [8, 9];
+//print 8 9 1
+console.log(p, q, r);
+*/
+
+// destructuriong objects
+
+const {name, openingHours, categories} = restaurant;
+
+console.log(name, openingHours, categories);
+
+const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+
+console.log(restaurantName, hours, tags);
+
+//default values
+
+const {menu = [], starterMenu: starters = []} = restaurant;
+console.log(menu, starters);
+
+//mutating variables
+let a = 111;
+let b = 999;
+const obj ={a:23, b:7, c:4};
+
+( {a, b} = obj);
+console.log(a, b);
+
+//nested objects
+
+const {fri: {open: o,close: c}} = openingHours;
+console.log(o, c);
