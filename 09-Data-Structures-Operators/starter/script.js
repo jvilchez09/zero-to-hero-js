a2b3fb9380e60580cc28fe842a0942187941c1b0
Jose Vilchez <jjva90@gmail.com>
2021-10-06 21:48:21 -0500

    map objects

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -307,3 +307,30 @@ const staff = ['waiter', 'chef', 'manager', 'chef', 'waiter'];
 
 const staffUnique = [...new Set(staff)];
 console.log(staffUnique);
+
+console.log('********MAPS');
+
+const rest = new Map();
+rest.set('name', 'clasico italiano');
+rest.set(1, 'Italy');
+console.log(rest.set(1, 'Portu'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'we are open')
+  .set(false, 'we close');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+const time = 7;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+/**
+ * Methods for maps:
+ * .has
+ * .delete
+ * .size
+ */
