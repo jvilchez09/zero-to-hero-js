1ee486c263afab633c26a7d865b75cb88d15ae2b
Jose Vilchez <jjva90@gmail.com>
2021-10-10 20:45:10 -0500

    ref vs value

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -4,7 +4,7 @@
  * advance topics about functions
  * Default parameter
  */
-
+/*
 const bookings = [];
 const createBooking = function (flightNum, numPass = 1, price = 199 * numPass) {
   const booking = {
@@ -21,3 +21,34 @@ createBooking('LH123', 2, 800);
 createBooking('LH124', 5);
 
 createBooking('LH124', undefined, 5000);
+*/
+
+/**
+ * Value vs reference
+ */
+
+const flight = 'LH123';
+const jose = {
+  name: 'jose Vil',
+  passport: 123548415,
+};
+
+const checkIn = function (flightNum, pass) {
+  flightNum = 'LH987';
+  pass.name = 'Mr.' + pass.name;
+  if (pass.passport === 123548415) {
+    console.log('Check IN');
+  } else {
+    console.log("CheckInn't");
+  }
+};
+
+checkIn(flight, jose);
+console.log(flight);
+console.log(jose);
+
+//passing by value
+/**
+ * Pass by reference es una referencia al valor en la memoria (objetos y arreglos)
+ * pasar por valor es una copia del valor (primitivos <todo lo demas>)
+ */
