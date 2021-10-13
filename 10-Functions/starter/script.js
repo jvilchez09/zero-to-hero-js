655c09714d6f31a3a5a4a6480c4f4b9bc64f593b
Jose Vilchez <jjva90@gmail.com>
2021-10-12 21:08:45 -0500

    higher order fn

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -26,7 +26,7 @@ createBooking('LH124', undefined, 5000);
 /**
  * Value vs reference
  */
-
+/*
 const flight = 'LH123';
 const jose = {
   name: 'jose Vil',
@@ -46,9 +46,33 @@ const checkIn = function (flightNum, pass) {
 checkIn(flight, jose);
 console.log(flight);
 console.log(jose);
-
+*/
 //passing by value
 /**
  * Pass by reference es una referencia al valor en la memoria (objetos y arreglos)
  * pasar por valor es una copia del valor (primitivos <todo lo demas>)
  */
+
+/**
+ * first class functions (first class citizens)
+ * higher order functions
+ */
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+//higher order function
+const transformer = function (str, fn) {
+  console.log(`Transformed str: ${fn(str)}`); //fn is the callback function
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('js is the best', upperFirstWord);
+
+transformer('js is the best', oneWord);
