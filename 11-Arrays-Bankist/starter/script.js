37d8baa08cefc3e41e90ad56bd31315244240f1d
Jose Vilchez <jjva90@gmail.com>
2021-10-20 21:42:32 -0500

    manipulating DOM Elements

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -120,3 +120,17 @@ currencies.forEach(function (value, key, map) {
   console.log(key);
   console.log(map);
 });
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = ` <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+    <div class="movements__value">${mov}</div>
+  </div>`;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
