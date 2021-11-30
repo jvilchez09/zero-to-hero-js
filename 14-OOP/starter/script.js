c0f203655189e093820132d2cd524ea6653a1269
Jose Vilchez <jjva90@gmail.com>
2021-11-29 20:06:50 -0500

    OOP JS

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+/**
+ * Constructor function
+ */
+const Person = function (firstName, birthYear) {
+  //instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  console.log(this);
+
+  //    never create methods inside a constructor function (to solve this we will use prototypes)
+  this.calcAge = function () {
+    console.log(2037 - this.birthYear);
+  };
+};
+
+const joe = new Person('Joe', 1990);
+console.log(joe);
+
+// 1. New is created
+//2. function is called, this {}
+//3. {} linked to prototype
+//4. function automatically return {}
+
+const matilda = new Person('Matilda', 1992);
+const charles = new Person('Charles', 1992);
+console.log(matilda, charles);
